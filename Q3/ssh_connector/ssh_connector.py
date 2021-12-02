0b44ee0cb7508588dab07c710c0fca8005c9d9d0
root <root@lyit-server-admin.lxd>
2021-12-02 22:55:50 +0000

    Modified original SSH connector file from Ruth example.

--- Q3/ssh_connector/ssh_connector.py
+++ Q3/ssh_connector/ssh_connector.py
@@ -1,27 +1,31 @@
 import paramiko
+from paramiko import SSHClient
 import time
 import re
 import sys
 
 
-commands = ['ls -al / > longList.txt\n']
+commands = ['hostname', 'ls -al / > longList.txt && ls -l longList.txt ', 'cat longList.txt']
 def ssh_connection(ip, port, username, password):
     try:
         print("Establishing a connection...")
         session = paramiko.SSHClient()
         session.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         session.connect(ip.rstrip("\n"), port, username, password)
-        connection = session.invoke_shell()
+        connection = session#.invoke_shell()
         for cmd in commands:
-            connection.send(cmd) #unix command to list directory contents and save to file
+            stdin, stdout, stderr  = connection.exec_command(cmd) #unix command to list directory contents and save to file
             time.sleep(1)
-            vm_output = connection.recv(65535)
-            print (vm_output)
-            if re.search(b"% Invalid input", vm_output):
+            vm_output = stdout.readlines()
+            vm_output = ''.join(vm_output)
+            
+            if  "Invalid input" in vm_output:  
                 print("There was at least one IOS syntax error on device {}".format(ip))
+                print (vm_output)
             else:
                 print("Commands successfully executed on {}".format(ip))
-            session.close()
+                print (vm_output)
+        session.close()
     except paramiko.AuthenticationException:
         print("Authentication Error")
 
