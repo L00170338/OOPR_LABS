cbd16898cff4eda9e2ee412c100aa15cf2e3ef2b
root <root@lyit-server-admin.lxd>
2021-12-03 00:45:53 +0000

    Added Q3 folder into repository

--- Q3/ssh_connector/ssh_connector.py
+++ Q3/ssh_connector/ssh_connector.py
@@ -1,3 +1,11 @@
+##################################################
+## Q2 - Assessment Webscrapper Apache Server.
+##################################################
+## Author: Wagner Ribeiro (L00170338).
+## Email: L00170338@student.lyit.ie
+## Status: Development.
+##################################################
+
 import paramiko
 from paramiko import SSHClient
 import time
